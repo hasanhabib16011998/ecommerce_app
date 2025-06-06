55a77a422e3b48a60cb614ba53e691db3fde00c6
hasanhabib16011998 <hasan16habib@gmail.com>
2025-06-06 17:48:01 +0600

    added navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
 import './App.css';
-import Header from './components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Header from './components/header/Header';
+import Navigation from './components/Navigation';
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Navigation/>
     </>
   )
 }
 
-export default App
+export default App;
